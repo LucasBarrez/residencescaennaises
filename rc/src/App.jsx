6f69059c76052a68956761498827defa2adf7bb8
LucasBarrez <lucasbarrez12@gmail.com>
2023-04-05 23:44:39 +0200

    👌 IMPROVE: pages export

--- rc/src/App.jsx
+++ rc/src/App.jsx
@@ -1,10 +1,31 @@
 import Header from "./components/Header/Header";
 import React from "react";
+import {Link, Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
+
+import Home from "./routes/Home";
+import Agrandissement from "./routes/Agrandissement";
+import Renovation from "./routes/Renovation";
+import Actu from "./routes/Actu";
+import Contact from "./routes/Contact";
+
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root/>}>
+        <Route index element={<Home/>}/>
+        <Route  element={<Construction/>}/>
+        <Route element={<Agrandissement/>}/>
+        <Route element={<Renovation/>}/>
+        <Route element={<Actu/>}/>
+        <Route element={<Contact/>}/>
+      </Route>
+    )
+  );
     return (
       <div className="App">
-        <Header/>
+        <RouterProvider router={router}/>
       </div>
     );
   }
---
+++ rc/src/routes/Agrandissement.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Agrandissement() {
+  return (
+    <div>Agrandissement</div>
+  )
+}
+
+export default Agrandissement
